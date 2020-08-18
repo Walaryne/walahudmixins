b137e20314231fa5f5621f9373ed63a30153794b
Walaryne <hazardoustheradioactive@gmail.com>
2020-08-18 02:45:53 -0400

    Added OW and NE coord lines

--- src/main/java/walaryne/walahudmixins/mixin/RenderHandlerMixin.java
+++ src/main/java/walaryne/walahudmixins/mixin/RenderHandlerMixin.java
@@ -2,6 +2,11 @@ package walaryne.walahudmixins.mixin;
 
 import fi.dy.masa.minihud.config.InfoToggle;
 import fi.dy.masa.minihud.event.RenderHandler;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.dimension.DimensionType;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,13 +21,25 @@ public abstract class RenderHandlerMixin {
 
     @Shadow(remap = false) private Set<InfoToggle> addedTypes;
 
+    @Final
+    @Shadow(remap = false)
+    private MinecraftClient mc = MinecraftClient.getInstance();
+
     @Inject(at = @At("TAIL"),
             method = "addLine(Lfi/dy/masa/minihud/config/InfoToggle;)V",
             remap = false)
     private void onAddLine(InfoToggle type, CallbackInfo ci) {
+        Entity entity = mc.getCameraEntity();
+        double y = entity.getBoundingBox().minY;
         if(this.addedTypes.contains(InfoToggle.COORDINATES)) {
             return;
         }
-        this.addLine("This is a test from an injection");
+
+        RegistryKey<DimensionType> dimension = mc.world.getDimensionRegistryKey();
+        if (dimension == DimensionType.THE_NETHER_REGISTRY_KEY) {
+            this.addLine(String.format("XYZ-OW: %.2f / %.4f / %.2f", entity.getX() * 8, y, entity.getZ() * 8));
+        } else if (dimension == DimensionType.OVERWORLD_REGISTRY_KEY) {
+            this.addLine(String.format("XYZ-NE: %.2f / %.4f / %.2f", entity.getX() / 8, y, entity.getZ() / 8));
+        }
     }
 }
