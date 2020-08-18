c16d139e9f0fbe6cf69e912a5165f8c662e61f17
Walaryne <hazardoustheradioactive@gmail.com>
2020-08-18 01:52:12 -0400

    Example line added

--- src/main/java/walaryne/walahudmixins/mixin/RenderHandlerMixin.java
+++ src/main/java/walaryne/walahudmixins/mixin/RenderHandlerMixin.java
@@ -3,16 +3,26 @@ package walaryne.walahudmixins.mixin;
 import fi.dy.masa.minihud.config.InfoToggle;
 import fi.dy.masa.minihud.event.RenderHandler;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Set;
+
 @Mixin(RenderHandler.class)
-public class RenderHandlerMixin {
-    @Inject(at = @At("HEAD"),
+public abstract class RenderHandlerMixin {
+    @Shadow(remap = false) protected abstract void addLine(String text);
+
+    @Shadow(remap = false) private Set<InfoToggle> addedTypes;
+
+    @Inject(at = @At("TAIL"),
             method = "addLine(Lfi/dy/masa/minihud/config/InfoToggle;)V",
-    remap = false)
+            remap = false)
     private void onAddLine(InfoToggle type, CallbackInfo ci) {
-        System.out.println("Injection is functioning.");
+        if(this.addedTypes.contains(InfoToggle.COORDINATES)) {
+            return;
+        }
+        this.addLine("This is a test from an injection");
     }
 }
